53ff866fe61c674e6ad0f121c102360a17e4062d
Тихончук Артём <artyom_tihonchuk_work@protonmail.com>
2022-05-06 19:08:53 +0300

    Добавьте файлы проекта.

---
+++ WordsCount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WordsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = String.Empty;
+            string[] txt;
+            StreamReader sr = new StreamReader("F:\\Downloads\\MEGA\\text_source.txt",Encoding.Default);
+            while (sr.EndOfStream != true)
+            {
+                s = sr.ReadLine();
+            }
+            txt = s.Split(' ');
+            Console.WriteLine("Количество слов:{0}",txt.Length);
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
